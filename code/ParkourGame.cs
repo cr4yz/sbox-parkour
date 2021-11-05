7c51f04986df1bc1a13eb7ebd48c8fe7a01fb1b9
Crayz <jacob.ee92@gmail.com>
2021-11-05 18:37:45 -0500

    First person camera and rename minimal shit to parkour

---
+++ code/ParkourGame.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+
+namespace Facepunch.Parkour
+{
+	public partial class ParkourGame : Sandbox.Game
+	{
+		public ParkourGame()
+		{
+			if ( IsClient )
+			{
+				new ParkourHud();
+			}
+		}
+
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new ParkourPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+
+}
